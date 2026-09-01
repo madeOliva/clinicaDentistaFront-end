9c23875f73b4fac07ef1e54e71eba99250220ae4
guzel03 <guzelalejandropaulinperez@gmail.com>
2026-09-01 17:13:07 -0400

    .

--- src/pages/Administrador.tsx
+++ src/pages/Administrador.tsx
@@ -9,8 +9,6 @@ const LOGIN_STORAGE_KEY = 'clinica-sonrisa-admin-login'
 const USUARIO = 'alex'
 const CONTRASEÑA = '1234'
 
-
-const Made = '1234'
 export default function Administrador() {
   const { servicios, agregarServicio, modificarServicio, eliminarServicio } = useServicios()
   const [form, setForm] = useState(formVacio)
